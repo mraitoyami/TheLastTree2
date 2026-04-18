38c5564dfde45a14a987f75ff8e8510c219b83e7
mraitoyami <mraitoyami@gmail.com>
2026-04-18 16:59:25 +0800

    Change THREE.js import to use CDN link

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 import { PlayerController, ThirdPersonCameraController } from './rosie/controls/rosieControls.js';
 import { CONFIG } from './config.js';
 import { Player } from './entities/Player.js';
